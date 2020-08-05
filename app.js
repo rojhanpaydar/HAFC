bd4cccd7225b2f2e1d9a898e1aa44802fb369690
Rojhan Paydar <rojhanpaydar@gmail.com>
2020-08-05 17:16:24 -0400

    added audio, and timing functions in js. Also adjusted the keyframes for the truck delay

---
+++ app.js
@@ -0,0 +1,12 @@
+setTimeout(function () {
+    document.getElementById("rev").play();
+    rev.volume = 0.1;
+}, 0)
+
+setTimeout(function () {
+    document.getElementById("honk").play();
+    honk.volume = 0.2;
+}, 2000)
+
+
+
