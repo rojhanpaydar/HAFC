61f952f815a76694400e58f2496ce52c51ab1236
Rojhan Paydar <rojhanpaydar@gmail.com>
2020-08-23 19:27:08 -0400

    added hamburger nav, need to adjust media queries

--- app.js
+++ app.js
@@ -8,5 +8,14 @@ setTimeout(function () {
     honk.volume = 0.2;
 }, 2500)
 
-
-
+const menuButton = document.querySelector('.menuButton');
+let menuOpen = false;
+menuButton.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuButton.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuButton.classList.remove('open');
+        menuOpen = false;
+    }
+})
