976b16769e52d205140bbbf24bda251fb2cd0384
Rojhan Paydar <rojhanpaydar@gmail.com>
2020-09-09 12:08:43 -0400

    our story page set up

--- app.js
+++ app.js
@@ -19,3 +19,27 @@ menuButton.addEventListener('click', () => {
         menuOpen = false;
     }
 })
+
+let menuToggle = document.querySelector("#menu-toggle");
+let menu = document.querySelector("#menu");
+
+menuToggle.addEventListener("click", function (event) {
+    var menuOpen = menu.classList.contains("active");
+    var newMenuOpenStatus = !menuOpen;
+
+    menuToggle.setAttribute("aria-expanded", newMenuOpenStatus);
+    menu.classList.toggle("active");
+});
+
+menuToggle.addEventListener("click", function (event) {
+    let menuOpen = menu.checked;
+
+    if (menuOpen) {
+        menu.checked = false;
+        menuToggle.setAttribute("aria-expanded", false);
+    } else {
+        menu.checked = true;
+        menuToggle.setAttribute("aria-expanded", true);
+    }
+});
+
